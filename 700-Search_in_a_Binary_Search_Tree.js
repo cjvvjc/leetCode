815ae7707182d04efa9b1f860a921f059e5e93b3
Chad Volk <cjvolk@gmail.com>
2022-12-04 14:07:00 -0700

    700-Search_in_a_Binary_Search_Tree

---
+++ 700-Search_in_a_Binary_Search_Tree.js
@@ -0,0 +1,21 @@
+/*
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+ var numJewelsInStones = function(jewels, stones) {
+
+    const map = {}
+    let count = 0
+  
+    for(const jewel of jewels){
+      map[jewel] = true
+    }
+  
+    for(const stone of stones){
+      if( map[stone] ){
+        count++
+      }
+    }
+    return count
+  };
